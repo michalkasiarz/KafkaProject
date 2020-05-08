4465f04f0d0b10940692800b943f22953576e93f
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-05-08 17:22:14 +0200

    producer java class added

---
+++ src/main/java/Producer.java
@@ -0,0 +1,25 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        int numOfRecords = 100;
+
+        for (int i = 0; i < numOfRecords; i++) {
+            producer.send(new ProducerRecord<>("numbers", Integer.toString(i), Integer.toString(i)));
+        }
+
+        producer.close();
+
+    }
+
+}
