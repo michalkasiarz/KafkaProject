80a32ecad93873c9034ff8c8f7636f4df234ca9c
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-05-13 17:41:32 +0200

    acknowledge property added

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -6,16 +6,18 @@ import java.util.Properties;
 public class Producer {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put("bootstrap.servers", "77.55.220.159:9092, 77.55.220.159:9093, 77.55.220.159:9094");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");
 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        KafkaProducer<Number, String> producer = new KafkaProducer<Number, String>(props);
 
         int numOfRecords = 100;
 
         for (int i = 0; i < numOfRecords; i++) {
-            producer.send(new ProducerRecord<>("numbers", Integer.toString(i), Integer.toString(i)));
+            System.out.println("Message " + i + " was just produced.");
+            producer.send(new ProducerRecord<Number, String>("numbers", i, Integer.toString(i)));
         }
 
         producer.close();
